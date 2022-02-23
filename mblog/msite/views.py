f43cb17c01050ae3e54a91d6519c69df0d587b81
AndyChiangSH <andy10801@gmail.com>
2022-02-23 14:08:07 +0800

    hw1 - mblog post tiile, body and pub_date

--- mblog/msite/views.py
+++ mblog/msite/views.py
@@ -1,3 +1,21 @@
+import imp
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
+from datetime import datetime
+
 
 # Create your views here.
+def homepage(request):
+    posts = Post.objects.all().order_by("-pub_date")
+    now = datetime.now()
+
+    return render(request, "index.html", locals())
+
+    # post_list = list()
+
+    # for i, post in enumerate(posts):
+    #     post_list.append(f"No.{i}: {post.title}<hr>")
+    #     post_list.append(f"<small>" + str(post.body.encode("utf-8").decode("utf-8")) + "</small><br><br>")
+
+    # return HttpResponse(post_list)
